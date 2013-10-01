d73f9fbb50b57b01ebaba0574648e39d9f895215
Aaron Ackerman <theron17@gmail.com>
2013-09-30 19:23:58 -0500

    Worked on added methods programatically to Bird.prototype

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 var request = require('request');
 var qs      = require('querystring');
+var _       = require('lodash');
+var routes  = require('./routes.json');
 
 var Bird = function() {
   this.hostname         = 'api.twitter.com';
-  this.requestTokenPath = this.hostname + '/oauth/request_token';
-  this.accessTokenPath  = this.hostname + '/oauth/access_token';
+  this.requestTokenPath = [this.hostname, 'oauth', 'request_token'].join('/');
+  this.accessTokenPath  = [this.hostname, 'oauth', 'access_token'].join('/');
   this.apiVersion       = '1.1';
 };
 
 Bird.prototype.login = function(options) {
   return request.post({
     url: this.requestTokenPath,
-    options: options.oauth
+    options: options
   });
 };
 
 Bird.prototype.auth = function(options) {
-  request.post({
+  return request.post({
     url: this.accessTokenPath,
-    options: options.oauth
+    options: options
   });
 };
+
+_.each(routes, function(methods, resource){
+  _.each(methods, function(routeHash, httpMethod){
+    _.each(routeHash, function(url, route){
+      Bird.prototype[resource] = Bird.prototype[resource] || {};
+      Bird.prototype[resource][route] = function(options){
+        return request[httpMethod]({
+          url: url,
+          options: options
+        });
+      };
+    });
+  });
+});
 
 module.exports.Bird = Bird;
