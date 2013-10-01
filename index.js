5feab23a5ce5cfb6fbcc5ca9b5fbf68bd788407d
Aaron Ackerman <theron17@gmail.com>
2013-09-30 20:27:11 -0500

    Make things more complex and add validations

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var request = require('request');
 var qs      = require('querystring');
 var _       = require('lodash');
 var routes  = require('./routes.json');
+var isValid = require('./vaidates').isValid;
 
 var Bird = function() {
   this.hostname         = 'api.twitter.com';
@@ -24,11 +25,32 @@ Bird.prototype.auth = function(options) {
   });
 };
 
+function buildUrl(url, options) {
+  var newUrl = url;
+  if (options.interpolate) {
+    options.interpolate = options.interpolate.replace(':', '');
+    newUrl = newUrl.replace(options.interpolate, options[options.interpolate]);
+  }
+  return newUrl + '.' + 'json';
+}
+
+// loop through each of the resources
 _.each(routes, function(methods, resource){
+  // ensure a namespace for each resource exists
+  Bird.prototype[resource] = Bird.prototype[resource] || {};
+
+  // loop through each method in the resources
   _.each(methods, function(routeHash, httpMethod){
-    _.each(routeHash, function(url, route){
-      Bird.prototype[resource] = Bird.prototype[resource] || {};
-      Bird.prototype[resource][route] = function(options){
+
+    // loop through the routes and add each route to the Bird prototype
+    _.each(routeHash, function(routeOptions, route){
+
+      // create methods for each route
+      Bird.prototype[resource][route] = function(){
+        if (routeOptions.validations) {
+          isValid(route, routeOptions.validations);
+        }
+        var url = buildUrl(routeOptions.url, options);
         return request[httpMethod]({
           url: url,
           options: options
