b714c7bb97b24edc25dab547086c17ab5881ecea
Aaron Ackerman <theron17@gmail.com>
2013-10-04 20:40:23 -0500

    Added test coverage for missing interpolation values

--- test/test-index.js
+++ test/test-index.js
@@ -4,7 +4,7 @@ var oauth  = require('../oauth.json');
 
 describe("Bird", function(){
 
-  describe("timeline.home", function(){
+  describe("timelines.home", function(){
     it("returns a valid json response", function(done){
       Bird.timelines.home({ oauth: oauth }, function(err, r, b){
         expect(r.statusCode).to.eq(200);
@@ -14,6 +14,15 @@ describe("Bird", function(){
 
     it("throws an error when not given oauth parameters", function(){
       expect(Bird.timelines.home).to.throw(Error);
+    });
+  });
+
+  describe("tweets.show", function(){
+    it("throws an error when not given an id parameter", function(){
+      var fn = function(){
+        Bird.tweets.show({ oauth: oauth });
+      }
+      expect(fn).to.throw(Error);
     });
   });
 });
